3c2d74476fd1ca2457b00a9f9f643ce177b9dffb
stherrien <stherrien@regscale.com>
2025-12-30 15:03:32 -0500

    feat: add GraphQL API layer with gqlgen
    - Schema: GraphQL schema for workflows, executions, credentials
    - Resolvers: Query and mutation resolvers
    - Converters: Model conversion between GraphQL and domain types
    - Config: gqlgen configuration for code generation

---
+++ internal/graphql/resolver.go
@@ -0,0 +1,24 @@
+package graphql
+
+import (
+	"log/slog"
+
+	"github.com/gorax/gorax/internal/schedule"
+	"github.com/gorax/gorax/internal/template"
+	"github.com/gorax/gorax/internal/webhook"
+	"github.com/gorax/gorax/internal/workflow"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require
+// here.
+
+// Resolver holds service dependencies for GraphQL resolvers
+type Resolver struct {
+	WorkflowService *workflow.Service
+	WebhookService  *webhook.Service
+	ScheduleService *schedule.Service
+	TemplateService *template.Service
+	Logger          *slog.Logger
+}
